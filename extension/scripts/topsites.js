0a4df9184b07fea8ee91b6cdaf9ac70b997b5798
Chris <65946592+idealistworld@users.noreply.github.com>
2022-09-09 01:39:10 -0400

    Mobile

--- extension/scripts/topsites.js
+++ extension/scripts/topsites.js
@@ -7,6 +7,14 @@ export function mostVisited(urls)
         var input = site.url.substring(8, site.url.length-1);
         url.innerHTML += input + " ";      
     }
+    var data = document.getElementById('top-sites-data');
+    for (var i = 0; i < 5; i++) 
+    {
+        var site = urls[i];
+        var input = site.url.substring(8, site.url.length-1);
+        var position = i + 1;
+        data.innerHTML += position + ". " + input + "<br>";      
+    }
 }
 
 chrome.topSites.get(mostVisited);
