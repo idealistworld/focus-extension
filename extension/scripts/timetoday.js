569590f20cba5c6a151ba33dac287990a6cc7a26
Chris <65946592+idealistworld@users.noreply.github.com>
2022-10-06 02:22:28 -0400

    Fixed Time Today Bugs

--- extension/scripts/timetoday.js
+++ extension/scripts/timetoday.js
@@ -1,37 +1,34 @@
-export function timeToday(db)
-{ var sortedList;
+export function timeToday(db) {
+    var sortedList;
     var totalTimeToday = 0;
-    for (var x = db.length -1; x > 0; x--)
-    {
-        var siteTimeToday= 0;
-        for (var i = db[x].unixTimeStamps.length - 1; i >= 0; i--)
-        {
+    for (var x = db.length - 1; x >= 0; x--) {
+        var siteTimeToday = 0;
+        for (var i = db[x].unixTimeStamps.length - 1; i >= 0; i--) {
             var databaseInput = db[x].unixTimeStamps;
-            if (Math.floor(Date.now() / 1000) - databaseInput[i] < 86400)
-            {
-                totalTimeToday += 1;      
-                siteTimeToday += 1;      
-            } 
+            if (Math.floor(Date.now() / 1000) - databaseInput[i] < 86400) {
+                totalTimeToday += 1;
+                siteTimeToday += 1;
+            }
         }
         db[x].unixTimeStamps["timeToday"] = siteTimeToday;
-       // alert( db[x].website + " " + db[x].unixTimeStamps.timeToday);
     }
     var displayData = document.getElementById('timeToday');
-    displayData.innerHTML = totalTimeToday/4 + " minutes";
+    displayData.innerHTML = totalTimeToday / 4 + " minutes";
 
     function dynamicSort(property) {
         var sortOrder = 1;
-        if(property[0] === "-") {
+        if (property[0] === "-") {
             sortOrder = -1;
             property = property.substr(1);
         }
-        return function (a,b) {
+        return function (a, b) {
             /* next line works with strings and numbers, 
              * and you may want to customize it to your needs
              */
-            var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+            var result = (a.unixTimeStamps[property] > b.unixTimeStamps[property]) ? -1 : (a.unixTimeStamps[property] < b.unixTimeStamps[property]) ? 1 : 0;
             return result * sortOrder;
-        }}
+        }
+    }
 
     var sortedList = db.sort(dynamicSort("timeToday"));
 
@@ -39,14 +36,11 @@ export function timeToday(db)
 
     var position = 1;
 
-    for (var i = 0; i < 6; i++) {
-        var siteTimeToday =  sortedList[i].unixTimeStamps.timeToday/4;
-        var siteUrl = sortedList[i].website
-        if(sortedList[i].website != "newtab"){
-            advancedData.innerHTML += position + ". " + siteUrl + " [" + siteTimeToday + " minutes]" + "<br>";
-            position +=1;
-        }
-      
+    for (var i = 0; i < 5; i++) {
+        var siteTimeToday = sortedList[i].unixTimeStamps.timeToday / 4;
+        var siteUrl = sortedList[i].website;
+        advancedData.innerHTML += position + ". " + siteUrl + " [" + siteTimeToday + " minutes]" + "<br>";
+        position += 1;
     }
 }
 
